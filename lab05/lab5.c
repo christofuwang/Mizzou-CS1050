e589a85e7f8c5e82f4636b643490af53f45483c3
Chris Wang <93212839+christofuwang@users.noreply.github.com>
2022-01-07 18:54:43 -0600

    Create lab5.c

---
+++ lab05/lab5.c
@@ -0,0 +1,34 @@
+#include <math.h>
+#include <stdio.h>
+
+//factorial function
+long factorial(int n){
+        long result = 1;
+        for (int i=n;i>1;i--)
+        {
+                result*=i;
+        }
+        return result;
+}
+
+//Calculates value of e with powers from 1-5
+double printe(double pownum){
+        int x = 1;
+        double one = 1;
+        double  e = 1;
+        for(x=1;x<50;x++){
+                e = e + (one/(factorial(x)));
+        }
+        double num = 0;
+        num = pow(e,pownum);
+        return (num);
+}
+
+//Main function print contents on printe
+int main(void){
+        int z = 0;
+        for(z = 1; z<6;z++){
+                printf("e to the %d power = %.3lf",z,printe(z));
+                printf("\n");
+        }
+}
